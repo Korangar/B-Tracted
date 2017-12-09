5b38330499e664e969616baa19b86ff5a0458646
XMG-II\fuhax <fuhax.myacc@gmx.de>
2017-12-09 04:30:24 +0100

    Basic Scripting: Input Selector Movement Essentials Game Behaviours

---
+++ Assets/Scritpting/InputBatch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputBatch : MonoBehaviour {
+	public string horizontal = "";
+	public string vertical = "";
+	public string accept = "";
+
+	public string cancel = "";
+
+	public string start = "";
+}
---
+++ Assets/Scritpting/PlayerBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	public static Vector3 move_v = new Vector3(0,0,1);
+	public static Vector3 move_h = new Vector3(1,0,0);
+	public static float moveDelay_s = 0.2f;
+	public static float deadzone = 0.9f;
+
+
+	public int resource = 100;
+	private InputBatch myInput; 
+
+	void Start()
+	{
+		myInput = GetComponent<InputBatch>();
+		StartCoroutine(SelectorMovement());
+	}
+
+	void Update()
+	{
+						
+		float v_abs = Mathf.Abs(Input.GetAxis(myInput.vertical));
+		float h_abs = Mathf.Abs(Input.GetAxis(myInput.horizontal));
+
+		if(v_abs > h_abs)
+			{
+				float sign = Mathf.Sign(Input.GetAxis(myInput.vertical));
+				Debug.DrawRay(transform.position, move_v*sign);
+			}
+			else
+			{
+				float sign = Mathf.Sign(Input.GetAxis(myInput.horizontal));
+				Debug.DrawRay(transform.position, move_h*sign);
+			}
+	}
+
+	private IEnumerator SelectorMovement()
+	{
+		while(true)
+		{
+			float v_in = 0f;
+			float h_in = 0f;
+			while(Mathf.Abs(v_in) < deadzone && Mathf.Abs(h_in) < deadzone) 
+			{
+				yield return null;
+				v_in = Input.GetAxis(myInput.vertical);
+				h_in = Input.GetAxis(myInput.horizontal);
+			}
+			
+			if(Mathf.Abs(v_in) >= Mathf.Abs(h_in))
+			{
+				transform.Translate(move_v*Mathf.Sign(v_in));
+			}
+			else
+			{
+				transform.Translate(move_h*Mathf.Sign(h_in));
+			}
+			yield return new WaitForSeconds(moveDelay_s);
+		}
+	} 
+
+}
