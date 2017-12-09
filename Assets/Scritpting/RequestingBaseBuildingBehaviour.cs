cbdde588afddbb26850020c94c7b0539921d98fa
Markus Gögele <markus.goegele@hotmail.com>
2017-12-09 16:33:01 +0100

    BeeBehaviour

--- Assets/Scritpting/RequestingBaseBuildingBehaviour.cs
+++ Assets/Scritpting/RequestingBaseBuildingBehaviour.cs
@@ -8,7 +8,7 @@ public class RequestingBaseBuildingBehaviour : BuildingBaseBehaviour
     {
     public int nmbrOfBees;
     public float requestDelay;
-    public List<Bee> beeList = new List<Bee>();
+    public List<BeeBehaviour> beeList = new List<BeeBehaviour>();
 
     private IEnumerator RequestBees()
     {
