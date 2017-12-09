af6931b5da7f797e449a4af550b69dd8aca06e62
LucaTuerk <lucatuerk@googlemail.com>
2017-12-09 05:02:23 +0100

    - Basic BarracksBehaviour

--- Assets/Scritpting/BarracksBehaviour.cs
+++ Assets/Scritpting/BarracksBehaviour.cs
@@ -1,16 +1,50 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BarracksBehaviour : MonoBehaviour {
+public class BarracksBehaviour : BuildingBaseBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    TileBehaviour targetTile;
+
+    private static int maxBees = 10;
+    private static float requestDelay = 1f;
+    private static float trainDelay = 5f; 
+
+    void Start(){
+        // StartCoroutine RequestBees
+    }
+
+    private void Update()
+    {
+        // if request bees finished, and not training
+            // StartCoroutine TrainBees
+    }
+
+    private IEnumerator RequestBees(){
+        int i = 0;
+
+        while(i < maxBees){
+            // REQUEST BEES
+            i++;
+            yield return new WaitForSeconds(requestDelay);
+        }
+    }
+
+    private IEnumerator TrainBees(){
+        int i = 0;
+        int bees = 0 /*number of current bees here*/; 
+
+        while(i < bees){
+            //trainBeeAtIndex(i);
+            i++;
+            yield return new WaitForSeconds(trainDelay);
+        }
+    }
+
+    public void Attack(TileBehaviour tile){
+        targetTile = tile;   
+    }
+
+    public void OnDefeat(){
+        
+    }
 }
