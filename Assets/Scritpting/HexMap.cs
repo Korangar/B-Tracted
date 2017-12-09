7468c889859d7ab8b3428e87b29dfa2fe660079e
Markus Gögele <markus.goegele@hotmail.com>
2017-12-09 15:10:21 +0100

    Coordinate Helper, Movement

--- Assets/Scritpting/HexMap.cs
+++ Assets/Scritpting/HexMap.cs
@@ -12,7 +12,6 @@ public class HexMap : MonoBehaviour {
 	public int NumColumns = 20;
 	public int NumRows = 15;
 
-
 	public void GenerateMap()
 	{	
 		TileBehaviour[] tiles = transform.GetComponentsInChildren<TileBehaviour>();
@@ -22,29 +21,17 @@ public class HexMap : MonoBehaviour {
 
 		for (int i = 0; i < NumColumns; i++) 
 		{
-
 			for (int j = 0; j < NumRows; j++) 
 			{
-				GameObject hex = (GameObject)Instantiate (hexagon, Position(i, j), Quaternion.identity);
+                Vector3 pos = Coordinate.Offset2Cube(new Vector2(i, j));
+                pos = Coordinate.Cube2Real(pos);
+
+                GameObject hex = (GameObject)Instantiate (hexagon, pos, Quaternion.identity);
 			//	hexesGO [hexagonScript].GetComponentInChildren<Hex> ().Hexagon = hexagonScript;
 				hex.name = i + "_" + j;
 			// Parent hexGO to Map
 				hex.transform.SetParent (this.transform);
 			}
 		}
-	}
-	public Vector3 Position(int Q, int R){
-		float radius = 1f;
-		float height = radius * 2;
-		float width = height * WIDTH_MULTIPLIER;
-
-		float vert = height * 0.75f;
-		float horiz = width;
-	
-		return new Vector3 (
-			horiz * (Q + (R & 1) / 2f), 
-			0, 
-			vert * R
-		);
 	}
 }
